fa998637616fc71130827a41c4e01e68f10ab5a1
João Contarim <joaocontar@hotmail.com>
2026-04-25 23:35:59 -0300

    Adição da estrutura backend e separação das stacks

---
+++ Backend/src/test/java/com/contarim/crud_spring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contarim.crud_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
